50b7497bfe11c3501f4d9fcb00efc32634eec4fe
Dmitriy <shadowpersik05@mail.ru>
2024-09-04 22:20:53 +0300

    [calc] Реализовал калькулятор для польской нотации (со скобками, если есть пробелы)

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 function calc(expression: string): number
 {
+    let brackets: number = 0;
     let stack: number[] = [];
-    let expressionElements: string[] = expression.split(/\s+/).reverse();
-    console.log(expressionElements);
+    let expressionElements: string[] = expression.split(/\s+/).reverse()
+    // решить вопрос со скобками
 
     expressionElements.forEach(element => {
+        if (brackets < 0)
+        {
+            console.log('Неверное расположение скобок');
+            return NaN;
+        }
         if (!isNaN(Number(element)))
         {
             stack.push(Number(element));
@@ -27,6 +33,12 @@ function calc(expression: string): number
                 case '/':
                     stack.push(first / second);
                     break;
+                case '(':
+                    brackets++;
+                    break;
+                case ')':
+                    brackets--;
+                    break;
                 default:
                     console.log(`Символ ${element} не число и не действие`);
                     return NaN;
@@ -34,10 +46,15 @@ function calc(expression: string): number
         }
 
     });
-    return stack.pop();
+    if (brackets !== 0)
+    {
+        return stack.pop();
+    }
+    console.log(`Неверное расположение скобок`);
+    return NaN;
 }
 
-const EXPRESSION = "+ 7 -2";
+const EXPRESSION = "* ( − 5 6 ) 7";
 let result = calc(EXPRESSION);
 if (!isNaN(result))
 {
